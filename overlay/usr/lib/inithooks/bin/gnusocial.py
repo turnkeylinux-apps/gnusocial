367057cfcb3cd31c43f874653574cf47d721198c
spaghettimaster <vlad.work.here@gmail.com>
2018-02-05 09:11:59 +0000

    Fix IP access issue (#958)

--- overlay/usr/lib/inithooks/bin/gnusocial.py
+++ overlay/usr/lib/inithooks/bin/gnusocial.py
@@ -4,8 +4,6 @@
 Option:
     --pass=     unless provided, will ask interactively
     --email=    unless provided, will ask interactively
-    --domain=   unless provided, will ask interactively
-                DEFAULT=www.example.com
 
 """
 
@@ -26,17 +24,15 @@ def usage(s=None):
     print >> sys.stderr, __doc__
     sys.exit(1)
 
-DEFAULT_DOMAIN="www.example.com"
 
 def main():
     try:
         opts, args = getopt.gnu_getopt(sys.argv[1:], "h",
-                                       ['help', 'pass=', 'email=', 'domain='])
+                                       ['help', 'pass=', 'email='])
     except getopt.GetoptError, e:
         usage(e)
 
     email = ""
-    domain = ""
     password = ""
     for opt, val in opts:
         if opt in ('-h', '--help'):
@@ -45,8 +41,6 @@ def main():
             password = val
         elif opt == '--email':
             email = val
-        elif opt == '--domain':
-            domain = val
 
     if not password:
         d = Dialog('TurnKey Linux - First boot configuration')
@@ -65,37 +59,12 @@ def main():
 
     inithooks_cache.write('APP_EMAIL', email)
 
-    if not domain:
-        if 'd' not in locals():
-            d = Dialog('TurnKey Linux - First boot configuration')
-
-        domain = d.get_input(
-            "GNU social Domain",
-            "Enter the domain to serve GNU social",
-            DEFAULT_DOMAIN)
-
-    if domain == "DEFAULT":
-        domain = DEFAULT_DOMAIN
-
-    inithooks_cache.write('APP_DOMAIN', domain)
-    
     hashpass = hashlib.md5(password + '1').hexdigest()   # userid
 
     m = MySQL()
     m.execute('UPDATE gnusocial.user SET email=\"%s\" WHERE nickname=\"administrator\";' % email)
     m.execute('UPDATE gnusocial.user SET password=\"%s\" WHERE nickname=\"administrator\";' % hashpass)
     m.execute('UPDATE gnusocial.user SET uri=\"http://%s/user/1\" WHERE nickname=\"administrator\";' % domain)
-
-    new = []
-    config = "/var/www/gnusocial/config.php"
-    for s in file(config).readlines():
-        s = s.rstrip()
-        s = re.sub("\['server'\](.*);", "['server'] = '%s';" % domain, s)
-        new.append(s)
-
-    fh = file(config, "w")
-    print >> fh, "\n".join(new)
-    fh.close()
 
 if __name__ == "__main__":
     main()
